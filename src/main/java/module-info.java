0baf5c4c16aaf2c520cc9bb9dcb4dcfc9a74f962
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2025-02-19 18:56:34 +0530

    removed test files

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module hellofx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires transitive javafx.graphics;
-    requires org.json;
-    
-    opens org.main.society to javafx.fxml;
-    exports org.main.society;
-}
