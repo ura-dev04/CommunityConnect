3a00d3d4d90af53ec802ffabea608a39e1252b96
Saima2704 <sskhan2704@gmail.com>
2025-03-11 23:39:26 +0530

    registration form storing data in firebase
    registration form storing data in firebase

---
+++ homepage.js
@@ -0,0 +1,8 @@
+document.querySelector('.cta-btn').addEventListener('click', () => {
+    alert('Welcome to CommunityConnect! Explore society management features.');
+  });
+  
+  document.querySelector('.login-btn').addEventListener('click', () => {
+    alert('Login feature coming soon!');
+  });
+  
