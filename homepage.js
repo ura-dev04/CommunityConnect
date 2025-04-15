098e7374f99fd7df64a0f270543c519a91adec84
Armaan Nakhuda <95076879+Armaan4477@users.noreply.github.com>
2025-04-15 12:43:58 +0530

    added face recog button

--- homepage.js
+++ homepage.js
@@ -5,3 +5,7 @@ document.querySelector('.cta-btn').addEventListener('click', () => {
 document.querySelector('.login-btn').addEventListener('click', () => {
   window.location.href = 'login.html';
 });
+
+document.querySelector('.verify-guest-btn').addEventListener('click', () => {
+  window.location.href = 'Face-Detection-JavaScript-master/verify-guest.html';
+});
